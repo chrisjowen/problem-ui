68120b5d2943218ecb5e39c69b27efb924460995
Chris Owen <chris.j.owen@hotmail.co.uk>
2023-06-11 23:30:10 +0800

    Adding the ability to generate new problem with links etc

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,6 +1,7 @@
 import { browser } from '$app/environment';
 import axios from 'axios';
 import { writable } from 'svelte/store';
+import api from './api';
 
 export const flags = writable({
 
@@ -26,6 +27,14 @@ export const state = writable({
 export const selectedProblem = writable({})
 
 
+
+
+
+api.sector.list("", 1, 100).then((res) => {
+    state.set({
+        sectors: res.data.entries
+    })
+});
 
 if(authValue.token) {
     axios.defaults.headers.common['Authorization'] = `bearer ${authValue.token}`
