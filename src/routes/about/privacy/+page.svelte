93da42be588ba058d4d778f7e0111ae8ddc131f2
Chris Owen <chris.j.owen@hotmail.co.uk>
2023-06-25 14:59:23 +0800

    Prevacy policy added

---
+++ src/routes/about/privacy/+page.svelte
@@ -0,0 +1,75 @@
+<div class="prose p-4 bg-white m-4 border max-w-[2000px]">
+  <h1>PRIVACY POLICY</h1>
+  <h2>1. INTRODUCTION</h2>
+
+  <p>
+    This privacy policy ("Policy") describes how Crowdsolve.AI ("Company", "we",
+    "us" or "our") collects, protects and uses the personally identifiable
+    information ("Personal Information") you ("User", "you" or "your") may
+    provide on the Crowdsolve.AI website and any of its products or services
+    (collectively, "Website" or "Services"). It also describes the choices
+    available to you regarding our use of your Personal Information and how you
+    can access and update this information. This Policy does not apply to the
+    practices of companies that we do not own or control, or to individuals that
+    we do not employ or manage.
+  </p>
+
+  <h2>2. COLLECTION OF PERSONAL INFORMATION</h2>
+
+  <p>
+    We receive and store any information you knowingly provide to us when you
+    create an account, publish content, make a purchase, or fill any online
+    forms on the Website. When required, this information may include your email
+    address, name, phone number, or other Personal Information. You can choose
+    not to provide us with certain information, but then you may not be able to
+    take advantage of some of the Website's features.
+  </p>
+
+  <h2>3. USE OF COLLECTED INFORMATION</h2>
+
+  <p>
+    Any of the information we collect from you may be used to personalize your
+    experience; improve our Website; improve customer service and respond to
+    queries and emails of our customers; send newsletters; send notification
+    emails such as password reminders, updates, etc; run and operate our Website
+    and Services.
+  </p>
+
+  <h2>4. GOOGLE OAUTH</h2>
+
+  <p>
+    We allow you to sign in using a Google account via Google OAuth. Google
+    OAuth is a feature that allows you to sign in to a website using your Google
+    credentials, and the website may receive information from your Google
+    profile such as your name, profile picture, country, and email address. You
+    can control the information that we receive from Google using the privacy
+    settings in your Google account. If you signed up for Crowdsolve.AI using
+    Google, we will securely store your Google ID to enable you to log in on
+    subsequent visits.
+  </p>
+
+  <h2>5. CHANGES TO THIS PRIVACY POLICY</h2>
+  <p>
+    We reserve the right to modify this privacy policy relating to the Website
+    or Services at any time, effective upon posting of an updated version of
+    this privacy policy on the Website. When we do, we will revise the updated
+    date at the bottom of this page. Continued use of the Website after any such
+    changes shall constitute your consent to such changes.
+  </p>
+
+  <h2>6. ACCEPTANCE OF THIS POLICY</h2>
+  <p>
+    You acknowledge that you have read this Policy and agree to all its terms
+    and conditions. By using the Website or its Services you agree to be bound
+    by this Policy. If you do not agree to abide by the terms of this Policy,
+    you are not authorized to use or access the Website and its Services.
+  </p>
+
+  <h2>7. CONTACTING US</h2>
+
+  <p>
+    If you have any questions about this Policy, please contact us at help@crowdsolve.ai
+  </p>
+
+  <p class="text-xs">This document was last updated on June 25, 2023</p>
+</div>
